33f6a3022438bf8458d24081b51cf1a63bbc981a
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-30 18:51:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution here:
-function driversWithRevenueOver(driver, revenue) {
+function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver){
     return driver.revenue > revenue;
   });
 }
 
-function driverNamesWithRevenueOver(driver, revenue) {
+function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(driver, revenue).map(function (driver){
     return driver.name;
   });
